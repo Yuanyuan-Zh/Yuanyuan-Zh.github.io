5c7f17a93724fc92813a4b53f9751d41eac380ca
Yuanyuan-Zh <91552633+Yuanyuan-Zh@users.noreply.github.com>
2026-03-02 14:20:21 +0000

    Deploying to gh-pages from @ Yuanyuan-Zh/Yuanyuan-Zh.github.io@d7f393e2cfd639ae694b737ce4acaa3dad43791c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "About",
+    id: "nav-yuanyuan-zhang",
+    title: "Yuanyuan Zhang",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
